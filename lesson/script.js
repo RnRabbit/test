d51dfa0b31383a091312f1ec36476e481399d6ef
Sergey <rnrabbit3@gmail.com>
2020-05-16 17:13:03 +0300

    Это наш первый коммит

---
+++ lesson/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// alert("Hello World!")
+
+// let answer = confirm("Are you here?");
+
+// console.log(answer);
+
+// let answer = +prompt("Есть ли вам 18?", "Да");
+
+// console.log(typeof(answer));
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log('2' === 2);
+
+let isChecked = true,
+    isClose = false;
+
+console.log(isChecked && !isClose);
